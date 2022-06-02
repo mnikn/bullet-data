30bbefb698ab11bc1a38a9ecdc2088e43e467106
mnikn <329121948@qq.com>
2022-06-02 13:53:51 +0800

    Update style.ts

--- src/renderer/style.ts
+++ src/renderer/style.ts
@@ -1,8 +1,8 @@
-export const PRIMARY_COLOR1 = '#FFF305';
-export const PRIMARY_COLOR1_LIGHT1 = '#FFF652';
+export const PRIMARY_COLOR1 = '#F0E96C';
+export const PRIMARY_COLOR1_LIGHT1 = '#F8D363';
 export const PRIMARY_COLOR2 = '#293033';
 export const PRIMARY_COLOR2_LIGHT1 = '#464D54';
 export const PRIMARY_COLOR2_LIGHT2 = '#80868C';
 
 // export const SECOND_COLOR1 = '#1FBEFF';
-export const SECOND_COLOR1 = '#FF1F47';
+export const SECOND_COLOR1 = '#B3465A';
